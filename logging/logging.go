860a659e3f0f2d10867540c4c3c458caa4f6e17a
辻気流 <kiryu.18011106@gmail.com>
2020-12-23 23:06:14 +0900

    feat:フィールド変数 update

--- logging/logging.go
+++ logging/logging.go
@@ -6,21 +6,37 @@ import (
 
 // Logging main struct
 type Logging struct {
-	format string
-	level  int
+	format    string
+	level     int
+	DEBUG     int
+	INFO      int
+	WARRING   int
+	ERROR     int
+	CRITICAL  int
+	levelList [5]string
 }
 
 // NewLogging constructor
 func NewLogging() *Logging {
 	logging := new(Logging)
 	logging.format = "%#v"
-	logging.level = 0
+	logging.DEBUG = 0
+	logging.INFO = 1
+	logging.WARRING = 2
+	logging.ERROR = 3
+	logging.CRITICAL = 4
+	logging.levelList[logging.DEBUG] = "debug"
+	logging.levelList[logging.INFO] = "info"
+	logging.levelList[logging.WARRING] = "warring"
+	logging.levelList[logging.ERROR] = "error"
+	logging.levelList[logging.CRITICAL] = "critical"
+	logging.level = logging.DEBUG
 	return logging
 }
 
 //SetLevel set logging print level
 func (l *Logging) SetLevel(level int) {
-	l.level = level
+	l.levelNow = level
 }
 
 //SetFormat set logging print  format
@@ -30,35 +46,35 @@ func (l *Logging) SetFormat(format string) {
 
 //Debug row level print
 func (l *Logging) Debug(text string) {
-	if 0 >= l.level {
+	if 0 >= l.levelNow {
 		log.Printf(l.format, text)
 	}
 }
 
 //Info row level print
 func (l *Logging) Info(text string) {
-	if 0 >= l.level {
+	if 0 >= l.levelNow {
 		log.Printf(l.format, text)
 	}
 }
 
 //Warning row level print
 func (l *Logging) Warning(text string) {
-	if 0 >= l.level {
+	if 0 >= l.levelNow {
 		log.Printf(l.format, text)
 	}
 }
 
 //Error row level print
 func (l *Logging) Error(text string) {
-	if 0 >= l.level {
+	if 0 >= l.levelNow {
 		log.Printf(l.format, text)
 	}
 }
 
 //Critical row level print
 func (l *Logging) Critical(text string) {
-	if 0 >= l.level {
+	if 0 >= l.levelNow {
 		log.Printf(l.format, text)
 	}
 }
