397ab24f54dbffa92809d6e6af21b0f3e3779107
辻気流 <kiryu.18011106@gmail.com>
2020-12-27 23:22:35 +0900

    test:Determine the type of the map

--- pprint/pprint.go
+++ pprint/pprint.go
@@ -1,38 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
-//pprint
-type pprint struct {
-	format_list []keyword
-	indent      int
-}
-
+//keyword set identifier
 type keyword struct {
 	identifier string
 	startWord  string
 	EndWord    string
 }
 
-//NewPrint construct
-func NewPrint() *pprint {
+//pprint
+type pprint struct {
+	formatList []keyword
+	indent     int
+}
+
+//NewPprint construct
+func NewPprint() *pprint {
 	pprint := new(pprint)
 	mapKey := keyword{"map", "{", "}"}
 	ArrayKey := keyword{"map", "{", "}"}
-	pprint.format_list = append(pprint.format_list, mapKey)
-	pprint.format_list = append(pprint.format_list, ArrayKey)
+	pprint.formatList = append(pprint.formatList, mapKey)
+	pprint.formatList = append(pprint.formatList, ArrayKey)
 	return pprint
 }
 
 //pprint print list slice
 func (p *pprint) pprint(data interface{}) {
 	switch v := data.(type) {
+	case map[string]string:
+		// fmt.Println("map")
+		fmt.Println(reflect.TypeOf(v))
+	case []string:
+		// fmt.Println("array")
+		fmt.Println(reflect.TypeOf(v))
 	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
+		// fmt.Println(v, "int")
+		fmt.Println(reflect.TypeOf(v))
 	default:
-		fmt.Printf("%v\n", v)
+		fmt.Println(reflect.TypeOf(v))
+		// fmt.Printf("%v[default]\n", v)
+		// fmt.Println(v)
 	}
 }
 
@@ -48,6 +59,10 @@ func (p *pprint) PrintArrays() {
 
 func main() {
 	m := map[string]string{"foo": "bar", "hello": "world"}
-	pprint := NewPrint()
+	n := map[int]string{1: "bar", 3: "world"}
+	a := 1
+	pprint := NewPprint()
 	pprint.pprint(m)
+	pprint.pprint(n)
+	pprint.pprint(a)
 }
