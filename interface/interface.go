9643ce80ca0e10f4cf9e677b8f38e505d21e8999
辻気流 <kiryu.18011106@gmail.com>
2021-01-13 11:49:48 +0900

    test:interface test

---
+++ interface/interface.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str interface{}
+	str = []int{0, 1, 2, 3, 4, 5, 6}
+	// var list []interface{}
+	// ret := make([]interface{}, str.Len)
+	// list = []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(str)
+	// fmt.Println(list)
+	mapOfSlices := map[string][]string{
+		"first":  {},
+		"second": []string{"one", "two", "three", "four", "five"},
+		"third":  []string{"quarter", "half"},
+	}
+	fmt.Println(mapOfSlices["third"])
+}
